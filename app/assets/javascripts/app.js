1d23d6ee463d99b2133a7050a2ab5d994f9b664b
briancmpbll <briancmpbll@gmail.com>
2016-03-07 13:17:54 -0800

    Move view templates into separate files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,15 @@
-angular.module('flapperNews', ['ui.router'])
+angular.module('flapperNews', ['ui.router', 'templates'])
 .config(['$stateProvider', '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '/home.html',
+        templateUrl: 'home/_home.html',
         controller: 'MainCtrl'
       })
       .state('posts', {
         url: '/posts/{id}',
-        templateUrl: '/posts.html',
+        templateUrl: 'posts/_posts.html',
         controller: 'PostsCtrl'
       });
 
